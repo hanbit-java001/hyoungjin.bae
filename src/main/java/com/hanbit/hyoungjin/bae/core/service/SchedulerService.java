47ba897a76bb0b5f3b2d98f0f90e961a9304468b
hyoungjinBae <frost_wyrm@naver.com>
2016-09-13 11:00:52 +0900

    2016/09/13 - 11시 deleteDAO까지

---
+++ src/main/java/com/hanbit/hyoungjin/bae/core/service/SchedulerService.java
@@ -0,0 +1,38 @@
+package com.hanbit.hyoungjin.bae.core.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import com.hanbit.hyoungjin.bae.core.dao.ScheduleDAO;
+import com.hanbit.hyoungjin.bae.core.vo.ScheduleVO;
+
+public class SchedulerService {
+	private ScheduleDAO scheduleDAO = new ScheduleDAO();
+
+	public int addSchedule(ScheduleVO schedule) {
+		return scheduleDAO.insertSchedule(schedule);
+	}
+
+	public int modifySchedule(ScheduleVO schedule) {
+		return scheduleDAO.updateSchedule(schedule);
+	}
+
+	public int removeSchedule(String scheduleId) {
+		return scheduleDAO.deleteSchedule(scheduleId);
+	}
+
+	public List<ScheduleVO> listSchedules(String startDt, String endDt) {
+
+		return null;
+	}
+
+	public ScheduleVO getSchedule(String scheduleId) {
+
+		return null;
+	}
+
+}

337202bdfaf65b77e2b5f34100c719de54d2328b
hyoungjinBae <frost_wyrm@naver.com>
2016-09-13 14:51:32 +0900

    selectSchedule

--- src/main/java/com/hanbit/hyoungjin/bae/core/dao/ScheduleDAO.java
+++ src/main/java/com/hanbit/hyoungjin/bae/core/dao/ScheduleDAO.java
@@ -3,6 +3,7 @@ package com.hanbit.hyoungjin.bae.core.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +15,13 @@ public class ScheduleDAO {
 		String url = "jdbc:oracle:thin:@127.0.0.1/xe";
 		String user = "hanbit";
 		String password = "hanbit";
+
 		Connection connection = null;
 
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 			connection = DriverManager.getConnection(url, user, password);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -30,36 +31,35 @@ public class ScheduleDAO {
 	private void closeConnection(Connection connection) {
 		try {
 			connection.close();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private int executeSql(Connection connection, String sql, List params){
+	private int executeSql(Connection connection, String sql, List params) {
 		int result = 0;
 
 		try {
 			PreparedStatement statement = connection.prepareStatement(sql);
 
-			for(int i=0; i<params.size(); i++){
-				statement.setObject(i+1, params.get(i));
+			for (int i = 0; i < params.size(); i++) {
+				statement.setObject(i + 1, params.get(i));
 			}
+
 			result = statement.executeUpdate();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return result;
 	}
 
-
 	public int insertSchedule(ScheduleVO schedule) {
 		Connection connection = getConnection();
+
 		String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) " + " VALUES(?, ?, ?, ?, ?)";
 
-		List params = new ArrayList();	// params 앞에 있는 List 는 인터페이스
+		List params = new ArrayList();
 		params.add(schedule.getScheduleId());
 		params.add(schedule.getTitle());
 		params.add(schedule.getMemo());
@@ -75,9 +75,11 @@ public class ScheduleDAO {
 
 	public int updateSchedule(ScheduleVO schedule) {
 		Connection connection = getConnection();
-		String sql = "UPDATE SCHEDULE SET TITLE=?, MEMO=?, START_DT=?, END_DT=?" + "WHERE SCHEDULE_ID=?";
 
-		List params = new ArrayList();	// params 앞에 있는 List 는 인터페이스
+		String sql = "UPDATE SCHEDULE SET TITLE = ?, MEMO = ?, " + "START_DT = ?, END_DT = ? "
+				+ "WHERE SCHEDULE_ID = ?";
+
+		List params = new ArrayList();
 		params.add(schedule.getTitle());
 		params.add(schedule.getMemo());
 		params.add(schedule.getStartDt());
@@ -91,11 +93,12 @@ public class ScheduleDAO {
 		return result;
 	}
 
-	public int deleteSchedule(String scheduleId){
+	public int deleteSchedule(String scheduleId) {
 		Connection connection = getConnection();
-		String sql = "DELETE FROM SCHEDULE WHERE SCHEDULE_ID=?";
 
-		List params = new ArrayList();	// params 앞에 있는 List 는 인터페이스
+		String sql = "DELETE FROM SCHEDULE WHERE SCHEDULE_ID = ?";
+
+		List params = new ArrayList();
 		params.add(scheduleId);
 
 		int result = executeSql(connection, sql, params);
@@ -105,5 +108,72 @@ public class ScheduleDAO {
 		return result;
 	}
 
+	public List<ScheduleVO> selectSchedules(String startDt, String endDt) {
+		Connection connection = getConnection();
+
+		String sql = "SELECT SCHEDULE_ID, TITLE, MEMO, " + "START_DT, END_DT FROM SCHEDULE "
+				+ "WHERE START_DT <= ? AND END_DT >= ?";
+
+		List params = new ArrayList();
+		params.add(endDt);
+		params.add(startDt);
+
+		List<ScheduleVO> result = new ArrayList<ScheduleVO>();
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			for (int i = 0; i < params.size(); i++) {
+				statement.setObject(i + 1, params.get(i));
+			}
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				ScheduleVO schedule = new ScheduleVO();
+
+				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
+				schedule.setTitle(resultSet.getString("TITLE"));
+				schedule.setMemo(resultSet.getString("MEMO"));
+				schedule.setStartDt(resultSet.getString("START_DT"));
+				schedule.setEndDt(resultSet.getString("END_DT"));
+
+				result.add(schedule);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		closeConnection(connection);
+
+		return result;
+	}
+
+	public ScheduleVO selectSchedule(String scheduleId) {
+		Connection connection = getConnection();
+		ScheduleVO schedule = new ScheduleVO();
+
+		String sql = "SELECT SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT FROM SCHEDULE "
+					+ "WHERE SCHEDULE_ID = ?";
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, scheduleId);
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
+				schedule.setTitle(resultSet.getString("TITLE"));
+				schedule.setMemo(resultSet.getString("MEMO"));
+				schedule.setStartDt(resultSet.getString("START_DT"));
+				schedule.setEndDt(resultSet.getString("END_DT"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		closeConnection(connection);
+		return schedule;
+	}
 
 }
